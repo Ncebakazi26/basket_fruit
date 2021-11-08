beeafaaff14b9d82f65f55cf0bb842204fd79f65
Ncebakazi <ncebakazi414@gmail.com>
2021-11-08 12:20:33 +0200

    fix tests

--- table.sql
+++ table.sql
@@ -8,6 +8,7 @@ create table fruit_basket (
 -- insert into fruit_basket ( fruit_name ,quantity,price) values('Apple',2,2.00);
 -- insert into fruit_basket ( fruit_name ,quantity,price) values('Peach',2,3.00);
 -- insert into fruit_basket ( fruit_name ,quantity,price) values('Pear',2,5.00);
--- insert into fruit_basket ( fruit_name ,quantity,price) values('Orange',5,3.00);
+insert into fruit_basket ( fruit_name ,quantity,price) values('Orange',5,3.00);
+insert into fruit_basket ( fruit_name ,quantity,price) values('Orange',10,30.00);
 
 -- update fruit_basket set quantity = 5 where fruit_name = 'Orange';
